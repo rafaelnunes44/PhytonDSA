6fab939a32a84a694df9feb7bf137a184265e2e7
Rafael Nunes <rafaelnunes44@gmail.com>
2020-05-06 14:35:01 -0300

    Listas

---
+++ Listas.py
@@ -0,0 +1,15 @@
+"""
+Listas !
+
+"""
+
+lista1 = [10, 20, 30, "Rafael", 50, 60]
+print(lista1)
+
+print("O nome Jolise está contida na lista ?")
+print("Jolise" in lista1)
+
+
+lista1.append("Jolise")
+print("Agora o nome Jolise está contido na lista?")
+print("Jolise" in lista1)
