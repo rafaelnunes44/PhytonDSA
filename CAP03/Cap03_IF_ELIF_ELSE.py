4ea80b890d51d033d1b0e19d72bf3b6493132740
Rafael Nunes <rafaelnunes44@gmail.com>
2020-05-09 23:45:12 -0300

    IF,ELIF,ELSE

---
+++ CAP03/Cap03_IF_ELIF_ELSE.py
@@ -0,0 +1,11 @@
+
+a = int(input("Digite um numero: "))
+
+if a < 100:
+    print("{} é menor que 100".format(a))
+
+elif (a > 100) and (a < 1000):
+    print("{} é maior que 100".format(a))
+
+else:
+    print("{} é maior que 1000".format(a))
