5ab180997aaca8b884bb838c55f26b6943956300
Rafael Nunes <rafaelnunes44@gmail.com>
2020-05-12 22:51:33 -0300

    while, range

---
+++ CAP03/Cap03_Estrutura_Repeticao_Range.py
@@ -0,0 +1,25 @@
+#range(start,stop,step)
+
+#Imprimindo números pares entre 50 e 101
+"""
+for i in range (50,101,2):
+    print(i)
+    i += 2
+"""
+
+#Usando números negativos
+"""
+for i in range (0, -20, -2):
+    print(i)
+"""
+#Percorrendo uma lista
+""""
+lista = ["Morango", "Abacaxi", "Uva", "Banana"]
+lista_tamanho = len(lista)
+
+for i in range(0, lista_tamanho):
+    print(lista[i])
+"""
+
+#Tudo em Python é um objeto
+print(type(range(0, 3)))
