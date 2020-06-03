b60f1e39a553714c78acce789201f1e5652562ac
Rafael Nunes <rafaelnunes44@gmail.com>
2020-06-02 23:07:32 -0300

    PyPI

---
+++ CAP04/PyPi.py
@@ -0,0 +1,29 @@
+"""
+Repositório Python - PyPI
+
+https://pypi.org/
+
+pacote Keras = Deep Learning for humans
+pacote tensorflow  = machine learning
+"""
+
+"""
+import random
+
+print(random.choice(["Laranja","Goiaba","Uva"])) #faz uma escolha aleatória
+
+print(random.sample(range(100),10)) #imprimi uma amostra com dados
+"""
+"""
+import statistics
+
+dados = [5.0, 10.0, 5.0, 8.0, 4.3, 18.9, 22.5]
+
+print(statistics.median(dados))
+"""
+
+import os # Sistema operacional
+
+print(dir(os))
+
+
